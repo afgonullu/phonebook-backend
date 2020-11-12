a8b86a93dfb9c12d1dad52af9a1c00413e243307
afgonullu <af@gonul.lu>
2020-11-12 12:16:04 +0300

    feat: add validations to post request

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const phonebook = require("./phonebook")
 let { persons } = require("./phonebook")
 const app = express()
 
@@ -27,34 +28,54 @@ app.get("/api/persons/:id", (req, res) => {
   }
 })
 
+// status code 204 doesn't allow sending a message or a json. therefore set statuscode 200
 app.delete("/api/persons/:id", (req, res) => {
   const id = Number(req.params.id)
 
   const person = persons.find((person) => person.id === id)
 
   if (!person) {
-    res.status(404).send(`<p>no person with id: ${id}. Request to delete unsuccessful.</p>`)
+    res
+      .status(404)
+      .send(`<p>no person with id: ${id}. Request to delete unsuccessful.</p>`)
   } else {
-    persons = persons.filter(person => person.id != id)
-    res.status(204).send(`<div><p>Following person is deleted</p><p>${person}</p></div>`)
+    persons = persons.filter((person) => person.id != id)
+    res
+      .status(200)
+      .send(`<div><p>Following person is deleted</p><p>${person}</p></div>`)
   }
 })
 
+// status code 204 doesn't allow sending a message or a json. therefore set statuscode 200
 app.post("/api/persons", (req, res) => {
-  if(!req.body.name) {
-    res.send("you must provide a name").end()
+  if (!req.body.name) {
+    return res.status(200).json({ error: "you must provide a name" })
+  }
+
+  const regex = /^[A-z ]+$/
+  if (!regex.test(req.body.name)) {
+    return res.status(200).json({ error: "name can only have letters." })
+  }
+
+  if (!req.body.number) {
+    return res.status(200).json({ error: "you must provide a number" })
+  }
+
+  if (!persons.every((person) => person.name != req.body.name)) {
+    return res
+      .status(200)
+      .json({ error: "the name you entered is already in the phonebook" })
   }
 
   const newPerson = {
     name: req.body.name,
     number: Number(req.body.number) | 0,
-    id: Math.floor(Math.random()*10000000),
+    id: Math.floor(Math.random() * 10000000),
   }
 
   persons.push(newPerson)
 
-  res.status(200).send(`person is created with id ${newPerson.id}`)
-
+  return res.status(200).send(`person is created with id ${newPerson.id}`)
 })
 
 app.get("/info", (req, res) => {
