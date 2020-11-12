9180c29fe8e1d08405f7d458f06441fef2666b61
A Faruk Gonullu <af@gonul.lu>
2020-11-12 10:33:40 +0300

    feat: create info endpoint
    FSO: part3-step2

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # phonebook-backend
+
 Full Stack Open Part 3 Phonebook Backend
--- index.js
+++ index.js
@@ -4,8 +4,19 @@ const app = express()
 
 app.use(express.json())
 
+//data extraction
+const length = persons.length
+
+const date = new Date().toString()
+
+const info = [`Phonebook has info for ${length} people.`, date]
+
 app.get("/api/persons", (req, res) => {
   res.json(persons)
+})
+
+app.get("/info", (req, res) => {
+  res.send(`<div><p>${info[0]}</p><p>${info[1]}</p></div>`)
 })
 
 const PORT = 3001
