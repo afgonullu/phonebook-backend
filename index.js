cb498ae1a7d2803fe0dacc0ed90dab6ee6b07c22
A Faruk Gonullu <af@gonul.lu>
2020-11-12 10:42:29 +0300

    feat: create api/persons/:id end point
    FSO: part3-step3

--- index.js
+++ index.js
@@ -15,6 +15,18 @@ app.get("/api/persons", (req, res) => {
   res.json(persons)
 })
 
+app.get("/api/persons/:id", (req, res) => {
+  const id = Number(req.params.id)
+
+  const person = persons.find((person) => person.id === id)
+
+  if (!person) {
+    res.status(404).send(`<p>no person with id: ${id}</p>`)
+  } else {
+    res.json(person)
+  }
+})
+
 app.get("/info", (req, res) => {
   res.send(`<div><p>${info[0]}</p><p>${info[1]}</p></div>`)
 })
