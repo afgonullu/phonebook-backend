29a651c69ca63154031a9497c48329ee891d0886
A Faruk Gonullu <af@gonul.lu>
2020-11-13 12:09:34 +0300

    feat: add data sent to morgan console log
    FSO: part3-step8

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const morgan = require("morgan")
 let { persons } = require("./phonebook")
 const app = express()
 
+morgan.token("data", (req, res) => JSON.stringify(req.body))
+
 app.use(express.json())
-app.use(morgan("tiny"))
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :data")
+)
 
 //data extraction
 const length = persons.length
